04c3d35404bbac9949f2d45db44764eac1ba68a5
valentinesamuel <valentinesamuel2580@gmail.com>
2022-07-31 12:40:42 -0700

    added task 1 and 2 basic

---
+++ read_line.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char **argv)
+{
+	size_t bytes_read;
+	size_t nbytes = 100;
+	char *my_string;
+
+	printf("Please enter a line of text => ");
+
+	my_string = (char *) malloc(nbytes + 1);
+	bytes_read = getline(&my_string, &nbytes, stdin);
+
+	if (bytes_read == -1)
+	{
+		printf("Error !!");
+		return (-1);
+	}
+	else
+	{
+		printf("You typed => %s\n", my_string);
+		printf("%zu were read\n", bytes_read);
+	}
+	return (0);
+}
